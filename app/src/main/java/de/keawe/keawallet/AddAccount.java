30dae4458e6fd83057543b07647eb4d87b525907
Stephan Richter <github@keawe.de>
2018-11-15 12:04:26 +0100

    working on bank institute parser

--- app/src/main/java/de/keawe/keawallet/AddAccount.java
+++ app/src/main/java/de/keawe/keawallet/AddAccount.java
@@ -2,6 +2,11 @@ package de.keawe.keawallet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import de.keawe.keawallet.objects.CreditInstitute;
 
 public class AddAccount extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class AddAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_account);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            CreditInstitute[] institutes = CreditInstitute.getList(getAssets());
+        } catch (IOException e) {
+            Toast.makeText(this, R.string.institutes_read_error, Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/de/keawe/keawallet/objects/CreditInstitute.java
+++ app/src/main/java/de/keawe/keawallet/objects/CreditInstitute.java
@@ -8,11 +8,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CreditInstitute {
+    public CreditInstitute(String blz,String name, String location, String bic, String hbci_url){
+
+    }
+
     public static CreditInstitute[] getList(AssetManager assets) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(assets.open("blz.properties")));
         String line = null;
         while ((line = reader.readLine()) != null) {
-            Log.d("Institute", line);
+            String[] parts = line.split("=",2);
+            String blz = parts[0];
+            parts = parts[1].split("\\|");
+            if (blz.equals("12096597"));
+            for (String d:parts) {
+                Log.d("Institute", d);
+            }
+            break;
+            /*String name=parts[0];
+            String location=parts[1];
+            String bic=parts[2];
+            String hbci_url=parts[5];*/
+
         }
         reader.close();
         return null;
