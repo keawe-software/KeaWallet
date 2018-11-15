a8afb3fc4b3d489f21a2f7e779ca2836c87457b9
Stephan Richter <github@keawe.de>
2018-11-15 13:17:44 +0100

    working on institute form

--- app/src/main/java/de/keawe/keawallet/AddAccount.java
+++ app/src/main/java/de/keawe/keawallet/AddAccount.java
@@ -1,28 +1,105 @@
 package de.keawe.keawallet;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.io.IOException;
+import java.util.Vector;
 
 import de.keawe.keawallet.objects.CreditInstitute;
 
-public class AddAccount extends AppCompatActivity {
+public class AddAccount extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    Vector<CreditInstitute> institutes = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_account);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        try {
-            CreditInstitute[] institutes = CreditInstitute.getList(getAssets());
+        if (institutes == null) try {
+            institutes = CreditInstitute.getList(getAssets(), CreditInstitute.HBCI_ONLY);
+            institutes.insertElementAt(new CreditInstitute(null,getString(R.string.institute_dropdown_initial),null,null,null),0);
+            CreditInstitute[] inst_arr = institutes.toArray(new CreditInstitute[institutes.size()]);
+            ArrayAdapter<CreditInstitute> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, inst_arr);
+            Spinner instituteSelector = (Spinner) findViewById(R.id.instituteSelector);
+            instituteSelector.setAdapter(adapter);
+            instituteSelector.setOnItemSelectedListener(this);
         } catch (IOException e) {
             Toast.makeText(this, R.string.institutes_read_error, Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        CreditInstitute institute = institutes.get(position);
+        findViewById(R.id.institute_credentials_form).setVisibility(institute.hasUrl()?View.VISIBLE:View.INVISIBLE);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO: implement onNothingSelected
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "AddAccount Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://de.keawe.keawallet/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "AddAccount Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://de.keawe.keawallet/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
--- app/src/main/java/de/keawe/keawallet/objects/CreditInstitute.java
+++ app/src/main/java/de/keawe/keawallet/objects/CreditInstitute.java
@@ -1,36 +1,70 @@
 package de.keawe.keawallet.objects;
 
 import android.content.res.AssetManager;
-import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Vector;
 
 public class CreditInstitute {
-    public CreditInstitute(String blz,String name, String location, String bic, String hbci_url){
+    public static final boolean HBCI_ONLY = true;
+    private final String bic,blz,location,name,url;
 
+    public CreditInstitute(String blz,String name, String location, String bic, String hbciUrl){
+        this.name=name;
+        this.blz=blz;
+        this.location=location;
+        this.bic=bic;
+        this.url=hbciUrl;
     }
 
-    public static CreditInstitute[] getList(AssetManager assets) throws IOException {
+    public static Vector<CreditInstitute> getList(AssetManager assets) throws IOException {
+        return getList(assets,false);
+    }
+
+    public static Vector<CreditInstitute> getList(AssetManager assets, boolean hbciOnly) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(assets.open("blz.properties")));
+        Vector<CreditInstitute> institutes = new Vector<CreditInstitute>();
+
         String line = null;
         while ((line = reader.readLine()) != null) {
-            String[] parts = line.split("=",2);
-            String blz = parts[0];
-            parts = parts[1].split("\\|");
-            if (blz.equals("12096597"));
-            for (String d:parts) {
-                Log.d("Institute", d);
-            }
-            break;
-            /*String name=parts[0];
-            String location=parts[1];
-            String bic=parts[2];
-            String hbci_url=parts[5];*/
-
+            String[] parts = line.split("=", 2);
+            String blz = parts[0].trim();
+            parts = parts[1].split("\\|", -1);
+            String name = parts[0];
+            String location = parts[1];
+            String bic = parts[2];
+            String hbciUrl = parts[5];
+            if (!hbciOnly || !hbciUrl.isEmpty()) institutes.add(new CreditInstitute(blz, name, location, bic, hbciUrl));
         }
         reader.close();
-        return null;
+        Collections.sort(institutes, CreditInstitute.comparator());
+        return institutes;
+    }
+
+    private static Comparator<? super CreditInstitute> comparator() {
+        return new Comparator<CreditInstitute>() {
+            @Override
+            public int compare(CreditInstitute lhs, CreditInstitute rhs) {
+                return lhs.name.compareTo(rhs.name);
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        if (this.blz == null) return this.name;
+        return '('+this.blz+") "+this.name;
+    }
+
+    public String bic_url() {
+        return this.url;
+    }
+
+    public boolean hasUrl() {
+        return this.url != null;
     }
 }
