a4b64a14e3fd9db084e99a1e45ce9bbe52741a21
Stephan Richter <github@keawe.de>
2018-11-15 11:27:01 +0100

    added new Activity for adding accounts

--- app/src/main/java/de/keawe/keawallet/TransactionList.java
+++ app/src/main/java/de/keawe/keawallet/TransactionList.java
@@ -1,7 +1,10 @@
 package de.keawe.keawallet;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class TransactionList extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class TransactionList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_list);
+
+        ImageButton button = (ImageButton)findViewById(R.id.addAccountButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoAddAccount(v);
+            }
+        });
+    }
+
+    private void gotoAddAccount(View v) {
+        Intent addAccount = new Intent(this,AddAccount.class);
+        startActivity(addAccount);
     }
 }
