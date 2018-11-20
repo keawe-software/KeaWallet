960671748dedf2a4c6c4ee62592f05385948cbed
Stephan Richter <github@keawe.de>
2018-11-20 02:05:58 +0100

    working on month selector for transaction list

--- app/src/main/java/de/keawe/keawallet/TransactionList.java
+++ app/src/main/java/de/keawe/keawallet/TransactionList.java
@@ -15,6 +15,9 @@ import android.widget.Spinner;
 import org.iban4j.CountryCode;
 import org.iban4j.Iban;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Vector;
 
 import de.keawe.keawallet.objects.Globals;
@@ -23,19 +26,48 @@ import de.keawe.keawallet.objects.database.BankLogin;
 
 public class TransactionList extends AppCompatActivity {
 
+    private Calendar month;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Globals.setAppcontext(getApplicationContext());
         setContentView(R.layout.activity_transaction_list);
 
-        ImageButton button = (ImageButton)findViewById(R.id.addAccountButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        ImageButton addAccountButton = (ImageButton)findViewById(R.id.addAccountButton);
+        addAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 gotoAddAccount();
             }
         });
+
+        ImageButton fetchTransactionButton = (ImageButton) findViewById(R.id.updateButton);
+        fetchTransactionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoFetchTransactions();
+            }
+        });
+        
+        ImageButton rightButton = (ImageButton) findViewById(R.id.right_button);
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeMonth(+1);
+            }
+        });
+
+
+        ImageButton leftButton = (ImageButton) findViewById(R.id.left_button);
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeMonth(-1);
+            }
+        });
+
+        month = Calendar.getInstance();
 
     }
 
@@ -44,26 +76,9 @@ public class TransactionList extends AppCompatActivity {
         startActivity(addAccount);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater findMenuItems = getMenuInflater();
-        findMenuItems.inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        if (id == R.id.fetch_transactions) {
-            Intent fetchTransactions = new Intent(this,FetchTransactions.class);
-            startActivity(fetchTransactions);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void gotoFetchTransactions() {
+        Intent fetchTransactions = new Intent(TransactionList.this,FetchTransactions.class);
+        startActivity(fetchTransactions);
     }
 
     @Override
@@ -71,6 +86,9 @@ public class TransactionList extends AppCompatActivity {
         super.onResume();
         Globals.d("Resuming TransactionList");
 
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
+
+        changeMonth(0);
 
         Vector<BankAccount> accounts = new Vector<>();
         Vector<BankLogin> bankLogins = BankLogin.loadAll();
@@ -78,5 +96,15 @@ public class TransactionList extends AppCompatActivity {
         BankAccount[] accountArray = accounts.toArray(new BankAccount[accounts.size()]);
         Spinner accountSelector = (Spinner)findViewById(R.id.account_selector);
         accountSelector.setAdapter(new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,accountArray));
+    }
+
+    public void changeMonth(int d){
+        month.add(Calendar.MONTH,d);
+        Calendar now = Calendar.getInstance();
+        now.add(Calendar.MONTH,-1);
+        findViewById(R.id.right_button).setVisibility(month.after(now)?View.GONE:View.VISIBLE);
+
+        int dummy = month.get(Calendar.MONTH)+1;
+        setTitle(getString(R.string.app_name)+" - "+month.get(Calendar.YEAR)+"-"+(dummy<10?"0":"")+dummy);
     }
 }
