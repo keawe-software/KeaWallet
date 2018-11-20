7bb8a7acd4da50ded2a84dd35270abb1b7b882d1
Stephan Richter <github@keawe.de>
2018-11-20 02:54:20 +0100

    prepareing transaction list

--- app/src/main/java/de/keawe/keawallet/TransactionList.java
+++ app/src/main/java/de/keawe/keawallet/TransactionList.java
@@ -8,9 +8,11 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import org.iban4j.CountryCode;
 import org.iban4j.Iban;
@@ -23,6 +25,7 @@ import java.util.Vector;
 import de.keawe.keawallet.objects.Globals;
 import de.keawe.keawallet.objects.database.BankAccount;
 import de.keawe.keawallet.objects.database.BankLogin;
+import de.keawe.keawallet.objects.database.Transaction;
 
 public class TransactionList extends AppCompatActivity {
 
@@ -67,6 +70,20 @@ public class TransactionList extends AppCompatActivity {
             }
         });
 
+        final Spinner accountDropdown = (Spinner) findViewById(R.id.account_selector);
+        accountDropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Object item = accountDropdown.getSelectedItem();
+                if (item instanceof BankAccount) changeBankAccount((BankAccount)item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
         month = Calendar.getInstance();
 
     }
@@ -93,9 +110,20 @@ public class TransactionList extends AppCompatActivity {
         Vector<BankAccount> accounts = new Vector<>();
         Vector<BankLogin> bankLogins = BankLogin.loadAll();
         for (BankLogin login:bankLogins) accounts.addAll(login.accounts());
-        BankAccount[] accountArray = accounts.toArray(new BankAccount[accounts.size()]);
-        Spinner accountSelector = (Spinner)findViewById(R.id.account_selector);
-        accountSelector.setAdapter(new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,accountArray));
+        if (accounts.isEmpty()) {
+            findViewById(R.id.transaction_and_category_list).setVisibility(View.INVISIBLE);
+            findViewById(R.id.left_button).setVisibility(View.INVISIBLE);
+        } else {
+            findViewById(R.id.transaction_and_category_list).setVisibility(View.VISIBLE);
+            findViewById(R.id.left_button).setVisibility(View.VISIBLE);
+            BankAccount[] accountArray = accounts.toArray(new BankAccount[accounts.size()]);
+            Spinner accountSelector = (Spinner) findViewById(R.id.account_selector);
+            accountSelector.setAdapter(new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, accountArray));
+        }
+    }
+
+    public void changeBankAccount(BankAccount account){
+        System.out.println(account);
     }
 
     public void changeMonth(int d){
